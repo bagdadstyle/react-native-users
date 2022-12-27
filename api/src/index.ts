31ced094792e3aaf76d1b8cff1ef142bb91f2d37
bagdadstyle <mau20410@gmail.com>
2022-12-27 18:34:58 -0300

    add swagger

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import index from "./routes/index";
+import swaggerSpec from "./services/swaggerSpec";
+
+//SWAGGER
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
 
+//--------MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  "/api-doc",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 app.use(morgan("dev"));
 
 app.use("/", index);
